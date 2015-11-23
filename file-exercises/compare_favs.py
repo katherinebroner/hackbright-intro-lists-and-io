89c4850ca843e50d7f665eaee84e63e00bbf39d6
Katherine Broner <katherine.broner@gmail.com>
2015-11-22 17:03:55 -0800

    refactored def

--- file-exercises/compare_favs.py
+++ file-exercises/compare_favs.py
@@ -1,8 +1,14 @@
+#def convert_file_to_list(text_file):
+#   file_open = open(text_file)
+#   file_list = file_open.readlines()
+#   file_open.close()
+#   return file_list 
+# more concise code below
+
 def convert_file_to_list(text_file):
-    file_open = open(text_file)
-    file_list = file_open.readlines()
-    file_open.close()
-    return file_list 
+    with open (text_file) as file_open:
+        file_list = file_open.readlines()
+    return file_list
 
 list_sandi = convert_file_to_list("sandi_fav_foods.txt")
 list_katherine = convert_file_to_list("katherine_fav_foods.txt")
@@ -17,13 +23,13 @@ compare_favs(list_sandi, list_katherine)
 
 def compare_favs2(list1, list2):
     if list1[0] in list2:
-        print "same foods", list1[0]
+        print "We both love", list1[0]
     elif list1[1] in list2:
-        print "same foods again", list1[1]
+        print "We both love", list1[1]
     elif list1[2] in list2:
-        print "same foods a third time", list1[2]
+        print "We both love", list1[2]
     else:
-        print "no same"
+        print "Our favorite foods are different."
 
 compare_favs2(list_sandi, list_katherine)
 
